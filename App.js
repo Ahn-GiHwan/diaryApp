57fce13101840c7ad7195b0ecd383a4d90adc636
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-13 01:27:20 +0900

    feat: connect realm and use AppLoding

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
+import React, { useState } from "react";
+import Realm from "realm";
+import AppLoading from "expo-app-loading";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import styled from "styled-components/native";
 import Navigator from "./navigator";
 
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
+
 const App = () => {
+  const [ready, setReady] = useState(false);
+
+  const startLoading = async () => {
+    const realm = await Realm.open({
+      path: "DiaryDB",
+      schema: [FeelingSchema],
+    });
+  };
+
+  const onFinish = () => setReady(true);
+
+  if (!ready)
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
   return (
     <NavigationContainer>
       <Navigator />
